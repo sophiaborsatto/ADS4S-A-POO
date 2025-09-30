8252977e381438b43e80bf976e62970ae7e6328b
unknown <sophia-borsatto@hotmail.com>
2025-09-29 22:27:56 -0300

    Service criado

--- FirstCrud/src/main/java/com/unicesumar/FirstCrud/repository/PessoaRepository.java
+++ FirstCrud/src/main/java/com/unicesumar/FirstCrud/repository/PessoaRepository.java
@@ -1,5 +1,6 @@
 package com.unicesumar.FirstCrud.repository;
 
+import com.unicesumar.FirstCrud.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
--- FirstCrud/src/main/java/com/unicesumar/FirstCrud/service/PessoaService.java
+++ FirstCrud/src/main/java/com/unicesumar/FirstCrud/service/PessoaService.java
@@ -1,4 +1,32 @@
 package com.unicesumar.FirstCrud.service;
 
+import com.unicesumar.FirstCrud.controller.PessoaController;
+import com.unicesumar.FirstCrud.model.Pessoa;
+import com.unicesumar.FirstCrud.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PessoaService {
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public List<Pessoa> listar(){
+        return pessoaRepository.findAll();
+    }
+
+    public Pessoa criarPessoa(Pessoa pessoa){
+        return pessoaRepository.save(pessoa);
+    }
+
+    public Pessoa buscarPorId(Long id){
+        return pessoaRepository.findById(id).
+                orElseThrow(() -> new RuntimeException("Pessoa não encontrada."));
+    }
+
+    public void deletarPessoa(Long id){
+        pessoaRepository.deleteById(id);
+    }
 }
